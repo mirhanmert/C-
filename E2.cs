16becf59af8465cfe2a6faba6613c08dc677a592
Mirhan Mert Sever <154518529+mirhanmert@users.noreply.github.com>
2023-12-25 20:05:48 +0300

    Decision Making Exercise 1
    This is a program that decides if the given year is a leap year or not.

---
+++ E2.cs
@@ -0,0 +1,39 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a year");
+
+        int year = Convert.ToInt32(Console.ReadLine());
+        if(year %4 ==0)
+        {
+            if (year %100 == 0)
+            {
+                if (year %400 == 0)
+                {
+                    Console.WriteLine(year + " is a leap year");
+                }
+                else
+                {
+                    Console.WriteLine(year + " is not a leap year");
+                } 
+
+            }
+            else
+            {
+                Console.WriteLine(year + " is a leap year");
+
+            }
+
+        }
+        else
+        {
+            Console.WriteLine(year + " is not a leap year");
+        }
+            
+
+
+    }
+
+}
