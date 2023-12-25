b7b003bf824df10029f1b00f85b28c929f126216
Mirhan Mert Sever <154518529+mirhanmert@users.noreply.github.com>
2023-12-25 20:03:25 +0300

    Decision Making Exercise 1
    This is a program that decides the age group based on the given age.

---
+++ E1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Your Age");
+        int age = Convert.ToInt32(Console.ReadLine());
+        if (age <= 12)
+        {
+            Console.WriteLine("Child");
+        }
+        else if (age <= 19) {
+            Console.WriteLine("Teenager");
+        }
+        else if (age <= 59)
+        {
+            Console.WriteLine("Adult");
+        }
+        else
+        {
+            Console.WriteLine("Elder");
+        }
+
+    }
+}
